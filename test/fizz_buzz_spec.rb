847c4bc5454d2288c0d1bae44c8ce6e2e0958dd4
bln23 <belen@nextail.co>
2020-11-20 10:11:04 +0100

    fix

---
+++ test/fizz_buzz_spec.rb
@@ -0,0 +1,8 @@
+require './fizz_buzz_spec'
+
+describe "FizzBuzz" do
+
+  it "foo" do
+    expect(true).to eq true
+  end
+end
--- test/gilded_rose_spec.rb
+++
@@ -1,8 +0,0 @@
-require './gilded_rose'
-
-describe "GildedRose" do
-
-  it "foo" do
-    expect(true).to eq false
-  end
-end
